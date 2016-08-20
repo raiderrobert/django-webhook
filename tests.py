08b8fb2f1129861492eea0305e4843f3b3445bdd
Robert Roskam <raiderrobert@gmail.com>
2016-08-20 17:52:20 -0400

    Fixed formatting

--- tests.py
+++ tests.py
@@ -35,20 +35,20 @@ urlpatterns = [
 ]
 
 
--# Tests start here
- -
- -class TestWebhook(TestCase):
- -
- -  def setUp(self):
- -      """initialize the Django test client"""
- -      self.c = Client()
- -        
- -  def test_your_test(self):
- -      python_dict = {
- -          "eventId": "5c0007",
- -          "portalId": 999,
- -          "userEmail": "fake@email.com"
- -      }
- -      response = self.c.post('/webhook-receiver/',
- -                                  json.dumps(python_dict),
- -                                  content_type="application/json")
+# Tests start here
+class TestWebhook(TestCase):
+
+    def setUp(self):
+        """initialize the Django test client"""
+        self.c = Client()
+            
+    def test_your_test(self):
+        python_dict = {
+            "eventId": "5c0007",
+            "portalId": 999,
+            "userEmail": "fake@email.com"
+        }
+        
+        response = self.c.post('/webhook-receiver/',
+                                json.dumps(python_dict),
+                                content_type="application/json")
