04928bc0f48917e98a4377285f143a190e795bca
Robert Roskam <raiderrobert@gmail.com>
2016-08-20 17:24:07 -0400

    Added tests back in

--- tests.py
+++ tests.py
@@ -33,3 +33,22 @@ class WebhookView(WebhookBase):
 urlpatterns = [
     url(r'^webhook-receiver', WebhookView.as_view(), name='web_hook'),
 ]
+
+
+-# Tests start here
+ -
+ -class TestWebhook(TestCase):
+ -
+ -  def setUp(self):
+ -      """initialize the Django test client"""
+ -      self.c = Client()
+ -        
+ -  def test_your_test(self):
+ -      python_dict = {
+ -          "eventId": "5c0007",
+ -          "portalId": 999,
+ -          "userEmail": "fake@email.com"
+ -      }
+ -      response = self.c.post('/webhook-receiver/',
+ -                                  json.dumps(python_dict),
+ -                                  content_type="application/json")
