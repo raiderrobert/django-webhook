3232831795316e89bf51ac6843991608ddf29b13
Robert Roskam <raiderrobert@gmail.com>
2016-09-10 20:57:49 -0400

    Create test_base.py

---
+++ tests/test_base.py
@@ -0,0 +1,21 @@
+from django.test import TestCase
+from django.test.client import Client
+
+
+class TestWebhook(TestCase):
+
+    def setUp(self):
+        """initialize the Django test client"""
+        self.c = Client()
+            
+    def test_your_test(self):
+        python_dict = {
+            "eventId": "5c0007",
+            "portalId": 999,
+            "userEmail": "fake@email.com"
+        }
+        
+        response = self.c.post('/webhook-receiver/',
+                                json.dumps(python_dict),
+                                content_type="application/json")
+        self.assertEqual(self.response.status_code, 200)
