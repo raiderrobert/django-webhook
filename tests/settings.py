6e7639bc318b55eca2c9215f73f1075dd1e9b609
Robert Roskam <raiderrobert@gmail.com>
2016-09-11 22:28:14 -0400

    Update settings.py

--- tests/settings.py
+++ tests/settings.py
@@ -5,8 +5,8 @@ Testing mini-project and tests in one
 from __future__ import unicode_literals
 from django.conf import settings
 
- if __name__ == '__main__':
-      settings.configure()
+if __name__ == '__main__':
+    settings.configure()
 
 DEBUG = True
 ROOT_URLCONF = 'tests.urls'
