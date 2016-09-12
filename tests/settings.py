3c8cccfa92e782004661046eb965396d70f013f2
Robert Roskam <raiderrobert@gmail.com>
2016-09-11 22:34:55 -0400

    Update settings.py

--- tests/settings.py
+++ tests/settings.py
@@ -5,10 +5,14 @@ Testing mini-project and tests in one
 from __future__ import unicode_literals
 from django.conf import settings
 
-if __name__ == '__main__':
-    settings.configure()
 
 DEBUG = True
 ROOT_URLCONF = 'tests.urls'
-DATABASES = {'default': {}}
 SECRET_KEY = "not so secret"
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    },
+}
