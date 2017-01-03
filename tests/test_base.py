c318ac1fbf5643580c88874e305c2bd1b290c7c1
Robert Roskam <raiderrobert@gmail.com>
2017-01-03 00:38:50 -0500

    Using context manager

--- tests/test_base.py
+++ tests/test_base.py
@@ -31,7 +31,8 @@ class TestIntegration(TestCase):
 class TestWebhookBase(TestCase):
     
     def test_unimplemented_process_webhook(self):
-        self.assertRaises(NotImplementedError, WebhookBase().process_webhook(data={}, meta={}))
+        with self.assertRaises(NotImplementedError):
+            WebhookBase().process_webhook(data={}, meta={}
 
     def test_unimplemented_handle_exception(self):
          WebhookBase().handle_exception(data={}, meta={})  # this should succeed
